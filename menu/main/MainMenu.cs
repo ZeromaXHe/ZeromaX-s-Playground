4e7b3eabab4840a4774cd492ffd6ac2d0263cbcf
Zhu XH <zhuxiaohe95@163.com>
2024-09-01 20:09:27 +0800

    init(HexGlobalGame): 初始化项目 - 国际化测试

---
+++ menu/main/MainMenu.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class MainMenu : Control
+{
+    private OptionButton _languageOptionButton;
+
+    public override void _Ready()
+    {
+        // on-ready
+        _languageOptionButton = GetNode<OptionButton>("%LanguageOptionButton");
+
+        _languageOptionButton.ItemSelected += index => SetLocale((int)index);
+
+        string locale = OS.GetLocale();
+        GD.Print(locale);
+        if (locale != null && locale.StartsWith("zh"))
+        {
+            _languageOptionButton.Selected = 0;
+        }
+        else
+        {
+            _languageOptionButton.Selected = 1;
+        }
+    }
+
+    private void SetLocale(int itemIndex)
+    {
+        string itemText = _languageOptionButton.GetItemText(itemIndex);
+        if ("中文".Equals(itemText))
+        {
+            TranslationServer.SetLocale("zh");
+        }
+        else
+        {
+            TranslationServer.SetLocale("en");
+        }
+    }
+}
