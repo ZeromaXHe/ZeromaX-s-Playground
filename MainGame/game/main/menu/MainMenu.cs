39d466e685ddda90fd162a34e54cdbc9a5cf28a2
Zhu XH <zhuxiaohe95@163.com>
2024-09-29 21:14:34 +0800

    feature(MainMenu): 主菜单按钮链接 Bilibili 和 GitHub

--- MainGame/game/main/menu/MainMenu.cs
+++ MainGame/game/main/menu/MainMenu.cs
@@ -7,17 +7,24 @@ public partial class MainMenu : Control
     private Button _startButton;
     private Button _quitButton;
 
+    private Button _bilibiliButton;
+    private Button _gitHubButton;
+
     public override void _Ready()
     {
         // on-ready
         _languageOptionButton = GetNode<OptionButton>("%LanguageOptionButton");
         _startButton = GetNode<Button>("CenterContainer/VBox/ButtonVBox/StartButton");
         _quitButton = GetNode<Button>("CenterContainer/VBox/ButtonVBox/QuitButton");
+        _bilibiliButton = GetNode<Button>("LbMarginContainer/VBox/BilibiliButton");
+        _gitHubButton = GetNode<Button>("LbMarginContainer/VBox/GitHubButton");
 
         _languageOptionButton.ItemSelected += index => SetLocale((int)index);
 
         _startButton.Pressed += OnStartButtonPressed;
         _quitButton.Pressed += OnQuitButtonPressed;
+        _bilibiliButton.Pressed += OnBilibiliButtonPressed;
+        _gitHubButton.Pressed += OnGitHubButtonPressed;
 
         string locale = OS.GetLocale();
         GD.Print(locale); // zh_CN
@@ -39,6 +46,16 @@ public partial class MainMenu : Control
     private void OnQuitButtonPressed()
     {
         GetTree().Quit();
+    }
+    
+    private void OnBilibiliButtonPressed()
+    {
+        OS.ShellOpen("https://space.bilibili.com/27867310");
+    }
+    
+    private void OnGitHubButtonPressed()
+    {
+        OS.ShellOpen("https://github.com/ZeromaXHe/ZeromaX-s-Playground");
     }
 
     private void SetLocale(int itemIndex)
