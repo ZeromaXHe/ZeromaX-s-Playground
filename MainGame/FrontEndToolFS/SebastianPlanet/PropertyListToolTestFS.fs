583a5a016c0b2c24d119a3aad6c7b62bb839ef26
Zhu XH <zhuxiaohe95@163.com>
2024-12-27 18:02:58 +0800

    test(SebastianPlanet): 测试 _GetPropertyList() 分组、usage

--- MainGame/FrontEndToolFS/SebastianPlanet/PropertyListToolTestFS.fs
+++ MainGame/FrontEndToolFS/SebastianPlanet/PropertyListToolTestFS.fs
@@ -6,10 +6,8 @@ open Godot.Collections
 
 // 参考 Godot 官方文档 _get_property_list() 示例：
 // https://docs.godotengine.org/zh-cn/4.x/classes/class_object.html#class-object-private-method-get-property-list
-// 实际貌似没有可读性和可用性。
-// 可读性：貌似这种实现方式没法分组，可读性差
-// 可用性：虽然解决了重载已保存场景时无法读取保存的自定义属性的问题，但编译后好像还是会读不出来（不确定 C# 版代码行不行，反正 F# 不行）
-// （可用性这一点存疑，现在发现其实不管是 Export 还是自定义，都是编译后得重新加载已保存场景才能读出来）
+// 可读性：能够使用 “/” 分割来实现分组
+// 可用性：不管是 Export 还是自定义，都是编译后得重新加载已保存场景才能读出来
 type PropertyListToolTestFS() =
     inherit Node3D()
 
@@ -21,10 +19,17 @@ type PropertyListToolTestFS() =
 
         for i in 0 .. this._numberCount - 1 do
             let items = new Dictionary()
-            items["name"] <- $"numbers_{i}"
+            items["name"] <- $"numbers/num{i}"
             items["type"] <- int Variant.Type.Int
             items["hint"] <- int PropertyHint.Enum
             items["hint_string"] <- "Zero,One,Two,Three,Four,Five"
+            let usage =
+                match i with
+                | 0 -> int PropertyUsageFlags.ReadOnly + int PropertyUsageFlags.Editor // 需要相加（||| 也行）
+                | 1 -> int PropertyUsageFlags.Editor
+                | 2 -> int PropertyUsageFlags.NoEditor // 不显示
+                | _ -> int PropertyUsageFlags.Default
+            items["usage"] <- usage
             properties.Add items
 
         properties
@@ -32,8 +37,8 @@ type PropertyListToolTestFS() =
     override this._Get property =
         let propertyName = property.ToString()
 
-        if propertyName.StartsWith("numbers_") then
-            let index = Int32.Parse << propertyName.Substring <| "numbers_".Length
+        if propertyName.StartsWith("numbers/num") then
+            let index = Int32.Parse << propertyName.Substring <| "numbers/num".Length
             Variant.CreateFrom this._numbers[index]
         else
             new Variant()
@@ -41,8 +46,8 @@ type PropertyListToolTestFS() =
     override this._Set(property, value) =
         let propertyName = property.ToString()
 
-        if propertyName.StartsWith("numbers_") then
-            let index = Int32.Parse << propertyName.Substring <| "numbers_".Length
+        if propertyName.StartsWith("numbers/num") then
+            let index = Int32.Parse << propertyName.Substring <| "numbers/num".Length
             // 不加这里的逻辑会 index 超过范围，无法加载保存的记录，官方文档中没有
             if index >= this._numbers.Count then
                 this._numbers.Resize(index + 1) |> ignore
