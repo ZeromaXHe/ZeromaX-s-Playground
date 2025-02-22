bbb310a880baa4ca9e1789d9c351f780458cb2bd
Zhu XH <zhuxiaohe95@163.com>
2025-02-22 16:43:25 +0800

    fix(MainProject): 修复缠绕方向问题 - 地形生成时，每个 Tile 的面顶点顺序正确性仅需判断一次

--- MainProject/Scenes/HexPlanet/Util/HexMesh.cs
+++ MainProject/Scenes/HexPlanet/Util/HexMesh.cs
@@ -31,10 +31,20 @@ public class HexMesh
     private void Triangulate(Tile tile)
     {
         var corners = tile.GetCorners(_radius + tile.Height, 1f).ToList();
+        var rightVSeq = Math3dUtil.IsRightVSeq(Vector3.Zero,
+            tile.GetCentroid(_radius + tile.Height), corners[0], corners[1]);
         for (var i = 0; i < corners.Count; i++)
-            Triangulate(tile, corners[i], corners[(i + 1) % corners.Count]);
+        {
+            var c1 = corners[i];
+            var c2 = corners[(i + 1) % corners.Count];
+            if (rightVSeq)
+                Triangulate(tile, c2, c1);
+            else
+                Triangulate(tile, c1, c2);
+        }
     }
 
+    // Godot 缠绕顺序是正面顺时针，所以从 c1 到 c2 相对于 tile 重心顺时针
     private void Triangulate(Tile tile, Vector3 c1, Vector3 c2)
     {
         var centroid = tile.GetCentroid(_radius + tile.Height);
@@ -63,8 +73,8 @@ public class HexMesh
     {
         var centroid = tile.GetCentroid(_radius + tile.Height);
         var neighbor = tile.GetNeighborByDirection(centroid.DirectionTo(c1), centroid.DirectionTo(c2));
-        // 连接将由更高的地块或相同高度时 Id 更大的地块生成
-        if (tile.Height < neighbor.Height ||
+        // 连接将由更低的地块或相同高度时 Id 更大的地块生成
+        if (tile.Height > neighbor.Height ||
             (Mathf.Abs(tile.Height - neighbor.Height) < 0.00001f && tile.Id < neighbor.Id)) return;
         var neighborCenter = neighbor.GetCenter(_radius + neighbor.Height);
         var cn1 = Math3dUtil.ProjectToSphere(c1, _radius + neighbor.Height);
@@ -78,21 +88,21 @@ public class HexMesh
             TriangulateEdgeStrip(e, tile.Color, en, neighbor.Color);
 
         var otherNeighbor1 = tile.GetNeighborsByDirection(c1 - centroid, neighbor.Id)[0];
-        if (tile.Height > otherNeighbor1.Height
+        if (tile.Height < otherNeighbor1.Height
             || (Mathf.Abs(tile.Height - otherNeighbor1.Height) < 0.00001f && tile.Id > otherNeighbor1.Id))
         {
-            // 连接角落的三角形由周围 3 个地块中最高或者一样高时 Id 最大的生成
+            // 连接角落的三角形由周围 3 个地块中最低或者一样高时 Id 最大的生成
             var onCentroid = otherNeighbor1.GetCentroid(_radius + otherNeighbor1.Height);
             var con1 = Math3dUtil.ProjectToSphere(c1, _radius + otherNeighbor1.Height);
             var von1 = onCentroid.Lerp(con1, HexMetrics.SolidFactor);
-            TriangulateCorner(e.V1, tile, vn1, neighbor, von1, otherNeighbor1);
+            TriangulateCorner(e.V1, tile, von1, otherNeighbor1, vn1, neighbor);
         }
 
         var otherNeighbor2 = tile.GetNeighborsByDirection(c2 - centroid, neighbor.Id)[0];
-        if (tile.Height > otherNeighbor2.Height
+        if (tile.Height < otherNeighbor2.Height
             || (Mathf.Abs(tile.Height - otherNeighbor2.Height) < 0.00001f && tile.Id > otherNeighbor2.Id))
         {
-            // 连接角落的三角形由周围 3 个地块中最高或者一样高时 Id 最大的生成
+            // 连接角落的三角形由周围 3 个地块中最低或者一样高时 Id 最大的生成
             var onCentroid = otherNeighbor2.GetCentroid(_radius + otherNeighbor2.Height);
             var con2 = Math3dUtil.ProjectToSphere(c2, _radius + otherNeighbor2.Height);
             var von2 = onCentroid.Lerp(con2, HexMetrics.SolidFactor);
@@ -100,78 +110,96 @@ public class HexMesh
         }
     }
 
-    private void TriangulateCorner(Vector3 top, Tile topTile, Vector3 low1, Tile lowTile1, Vector3 low2, Tile lowTile2)
+    // 需要保证入参 bottom -> left -> right 是顺时针
+    private void TriangulateCorner(Vector3 bottom, Tile bottomTile,
+        Vector3 left, Tile leftTile, Vector3 right, Tile rightTile)
     {
-        var edgeType1 = HexMetrics.GetEdgeType(topTile.Elevation, lowTile1.Elevation);
-        var edgeType2 = HexMetrics.GetEdgeType(topTile.Elevation, lowTile2.Elevation);
+        var edgeType1 = HexMetrics.GetEdgeType(bottomTile.Elevation, leftTile.Elevation);
+        var edgeType2 = HexMetrics.GetEdgeType(bottomTile.Elevation, rightTile.Elevation);
         if (edgeType1 == HexEdgeType.Slope)
         {
             if (edgeType2 == HexEdgeType.Slope)
-                TriangulateCornerTerraces(top, topTile, low1, lowTile1, low2, lowTile2);
+                TriangulateCornerTerraces(bottom, bottomTile, left, leftTile, right, rightTile);
             else if (edgeType2 == HexEdgeType.Flat)
-                TriangulateCornerTerraces(low1, lowTile1, top, topTile, low2, lowTile2);
+                TriangulateCornerTerraces(left, leftTile, right, rightTile, bottom, bottomTile);
             else
-                TriangulateCornerTerracesCliff(top, topTile, low1, lowTile1, low2, lowTile2);
+                TriangulateCornerTerracesCliff(bottom, bottomTile, left, leftTile, right, rightTile);
         }
         else if (edgeType2 == HexEdgeType.Slope)
         {
             if (edgeType1 == HexEdgeType.Flat)
-                TriangulateCornerTerraces(low2, lowTile2, top, topTile, low1, lowTile1);
+                TriangulateCornerTerraces(right, rightTile, bottom, bottomTile, left, leftTile);
             else
-                TriangulateCornerTerracesCliff(top, topTile, low2, lowTile2, low1, lowTile1);
+                TriangulateCornerCliffTerraces(bottom, bottomTile, left, leftTile, right, rightTile);
         }
-        else if (HexMetrics.GetEdgeType(lowTile1.Elevation, lowTile2.Elevation) == HexEdgeType.Slope)
+        else if (HexMetrics.GetEdgeType(leftTile.Elevation, rightTile.Elevation) == HexEdgeType.Slope)
         {
-            if (lowTile1.Elevation < lowTile2.Elevation)
-                TriangulateCornerTerracesCliff(low1, lowTile1, low2, lowTile2, top, topTile);
+            if (leftTile.Elevation < rightTile.Elevation)
+                TriangulateCornerCliffTerraces(right, rightTile, bottom, bottomTile, left, leftTile);
             else
-                TriangulateCornerTerracesCliff(low2, lowTile2, low1, lowTile1, top, topTile);
+                TriangulateCornerTerracesCliff(left, leftTile, right, rightTile, bottom, bottomTile);
         }
         else
-            AddTriangle([top, low1, low2], [topTile.Color, lowTile1.Color, lowTile2.Color]);
+            AddTriangle([bottom, left, right], [bottomTile.Color, leftTile.Color, rightTile.Color]);
     }
 
-    // 处理阶地高的 topTile 和相差 1 的阶地低的 lowTile、高度和前两者均不同的 tile（即三角形一边是阶地，一边是悬崖，一边任意）的情况
-    private void TriangulateCornerTerracesCliff(Vector3 top, Tile topTile, Vector3 low, Tile lowTile, Vector3 vec,
-        Tile tile)
+    // 三角形靠近 tile 的左边是阶地，右边是悬崖，另一边任意的情况
+    private void TriangulateCornerTerracesCliff(Vector3 begin, Tile beginTile,
+        Vector3 left, Tile leftTile, Vector3 right, Tile rightTile)
     {
-        var b = 1f / Mathf.Abs(topTile.Elevation - tile.Elevation);
-        var boundary = HexMetrics.Perturb(top).Lerp(HexMetrics.Perturb(vec), b);
-        var boundaryColor = topTile.Color.Lerp(tile.Color, b);
-        TriangulateBoundaryTriangle(top, topTile, low, lowTile, boundary, boundaryColor);
-        if (HexMetrics.GetEdgeType(lowTile.Elevation, tile.Elevation) == HexEdgeType.Slope)
-            TriangulateBoundaryTriangle(low, lowTile, vec, tile, boundary, boundaryColor);
+        var b = 1f / Mathf.Abs(rightTile.Elevation - beginTile.Elevation);
+        var boundary = HexMetrics.Perturb(begin).Lerp(HexMetrics.Perturb(right), b);
+        var boundaryColor = beginTile.Color.Lerp(rightTile.Color, b);
+        TriangulateBoundaryTriangle(begin, beginTile, left, leftTile, boundary, boundaryColor);
+        if (HexMetrics.GetEdgeType(leftTile.Elevation, rightTile.Elevation) == HexEdgeType.Slope)
+            TriangulateBoundaryTriangle(left, leftTile, right, rightTile, boundary, boundaryColor);
         else
-            AddTriangleUnperturbed([HexMetrics.Perturb(low), HexMetrics.Perturb(vec), boundary], [lowTile.Color, tile.Color, boundaryColor]);
+            AddTriangleUnperturbed([HexMetrics.Perturb(left), HexMetrics.Perturb(right), boundary],
+                [leftTile.Color, rightTile.Color, boundaryColor]);
+    }
+
+    // 三角形靠近 tile 的左边是悬崖，右边是阶地，另一边任意的情况
+    private void TriangulateCornerCliffTerraces(Vector3 begin, Tile beginTile,
+        Vector3 left, Tile leftTile, Vector3 right, Tile rightTile)
+    {
+        var b = 1f / Mathf.Abs(leftTile.Elevation - beginTile.Elevation);
+        var boundary = HexMetrics.Perturb(begin).Lerp(HexMetrics.Perturb(left), b);
+        var boundaryColor = beginTile.Color.Lerp(leftTile.Color, b);
+        TriangulateBoundaryTriangle(right, rightTile, begin, beginTile, boundary, boundaryColor);
+        if (HexMetrics.GetEdgeType(leftTile.Elevation, rightTile.Elevation) == HexEdgeType.Slope)
+            TriangulateBoundaryTriangle(left, leftTile, right, rightTile, boundary, boundaryColor);
+        else
+            AddTriangleUnperturbed([HexMetrics.Perturb(left), HexMetrics.Perturb(right), boundary],
+                [leftTile.Color, rightTile.Color, boundaryColor]);
     }
 
     // 阶地和悬崖中间的半三角形
-    private void TriangulateBoundaryTriangle(Vector3 top, Tile topTile, Vector3 low, Tile lowTile, Vector3 boundary,
-        Color boundaryColor)
+    private void TriangulateBoundaryTriangle(Vector3 begin, Tile beginTile,
+        Vector3 left, Tile leftTile, Vector3 boundary, Color boundaryColor)
     {
-        var v2 = HexMetrics.Perturb(HexMetrics.TerraceLerp(top, low, 1));
-        var c2 = HexMetrics.TerraceLerp(topTile.Color, lowTile.Color, 1);
-        AddTriangleUnperturbed([HexMetrics.Perturb(top), v2, boundary], [topTile.Color, c2, boundaryColor]);
+        var v2 = HexMetrics.Perturb(HexMetrics.TerraceLerp(begin, left, 1));
+        var c2 = HexMetrics.TerraceLerp(beginTile.Color, leftTile.Color, 1);
+        AddTriangleUnperturbed([HexMetrics.Perturb(begin), v2, boundary], [beginTile.Color, c2, boundaryColor]);
         for (var i = 2; i < HexMetrics.TerraceSteps; i++)
         {
             var v1 = v2;
             var c1 = c2;
-            v2 = HexMetrics.Perturb(HexMetrics.TerraceLerp(top, low, i));
-            c2 = HexMetrics.TerraceLerp(topTile.Color, lowTile.Color, i);
+            v2 = HexMetrics.Perturb(HexMetrics.TerraceLerp(begin, left, i));
+            c2 = HexMetrics.TerraceLerp(beginTile.Color, leftTile.Color, i);
             AddTriangleUnperturbed([v1, v2, boundary], [c1, c2, boundaryColor]);
         }
 
-        AddTriangleUnperturbed([v2, HexMetrics.Perturb(low), boundary], [c2, lowTile.Color, boundaryColor]);
+        AddTriangleUnperturbed([v2, HexMetrics.Perturb(left), boundary], [c2, leftTile.Color, boundaryColor]);
     }
 
     // 处理高度不同的 beginTile 和两个高度相同的 endTile（即三角形两边是等高阶地，一边是平地）的情况
-    private void TriangulateCornerTerraces(Vector3 begin, Tile beginTile, Vector3 end1, Tile endTile1, Vector3 end2,
-        Tile endTile2)
+    private void TriangulateCornerTerraces(Vector3 begin, Tile beginTile,
+        Vector3 left, Tile leftTile, Vector3 right, Tile rightTile)
     {
-        var v3 = HexMetrics.TerraceLerp(begin, end1, 1);
-        var v4 = HexMetrics.TerraceLerp(begin, end2, 1);
-        var c3 = HexMetrics.TerraceLerp(beginTile.Color, endTile1.Color, 1);
-        var c4 = HexMetrics.TerraceLerp(beginTile.Color, endTile2.Color, 1);
+        var v3 = HexMetrics.TerraceLerp(begin, left, 1);
+        var v4 = HexMetrics.TerraceLerp(begin, right, 1);
+        var c3 = HexMetrics.TerraceLerp(beginTile.Color, leftTile.Color, 1);
+        var c4 = HexMetrics.TerraceLerp(beginTile.Color, rightTile.Color, 1);
         AddTriangle([begin, v3, v4], [beginTile.Color, c3, c4]);
         for (var i = 0; i < HexMetrics.TerraceSteps; i++)
         {
@@ -179,14 +207,14 @@ public class HexMesh
             var v2 = v4;
             var c1 = c3;
             var c2 = c4;
-            v3 = HexMetrics.TerraceLerp(begin, end1, i);
-            v4 = HexMetrics.TerraceLerp(begin, end2, i);
-            c3 = HexMetrics.TerraceLerp(beginTile.Color, endTile1.Color, i);
-            c4 = HexMetrics.TerraceLerp(beginTile.Color, endTile2.Color, i);
+            v3 = HexMetrics.TerraceLerp(begin, left, i);
+            v4 = HexMetrics.TerraceLerp(begin, right, i);
+            c3 = HexMetrics.TerraceLerp(beginTile.Color, leftTile.Color, i);
+            c4 = HexMetrics.TerraceLerp(beginTile.Color, rightTile.Color, i);
             AddQuad([v1, v2, v3, v4], [c1, c2, c3, c4]);
         }
 
-        AddQuad([v3, v4, end1, end2], [c3, c4, endTile1.Color, endTile2.Color]);
+        AddQuad([v3, v4, left, right], [c3, c4, leftTile.Color, rightTile.Color]);
     }
 
     private void TriangulateEdgeTerraces(EdgeVertices begin, Tile beginTile, EdgeVertices end, Tile endTile)
@@ -215,12 +243,11 @@ public class HexMesh
 
     private void AddTriangleUnperturbed(Vector3[] vs, Color[] cs = null)
     {
-        var idx = Math3dUtil.SortVertices(Vector3.Zero, vs);
         for (var i = 0; i < 3; i++)
         {
             if (cs != null)
-                _surfaceTool.SetColor(cs[idx[i]]);
-            _surfaceTool.AddVertex(vs[idx[i]]);
+                _surfaceTool.SetColor(cs[i]);
+            _surfaceTool.AddVertex(vs[i]);
         }
 
         _surfaceTool.AddIndex(_vIdx);
@@ -234,12 +261,11 @@ public class HexMesh
 
     private void AddQuadUnperturbed(Vector3[] vs, Color[] cs = null)
     {
-        var idx = Math3dUtil.SortVertices(Vector3.Zero, vs);
         for (var i = 0; i < 4; i++)
         {
             if (cs != null)
-                _surfaceTool.SetColor(cs[idx[i]]);
-            _surfaceTool.AddVertex(vs[idx[i]]);
+                _surfaceTool.SetColor(cs[i]);
+            _surfaceTool.AddVertex(vs[i]);
         }
 
         _surfaceTool.AddIndex(_vIdx);
--- MainProject/Scenes/HexPlanet/Util/Math3dUtil.cs
+++ MainProject/Scenes/HexPlanet/Util/Math3dUtil.cs
@@ -20,28 +20,12 @@ public static class Math3dUtil
         return p * projectionPoint * scale;
     }
 
-
-    /// <summary>
-    /// 排序顶点，使缠绕方向朝外
-    /// </summary>
-    /// <param name="origin">在形状内部的一个起点</param>
-    /// <param name="vs">顶点数组（目前支持 3 个和 4 个）</param>
-    /// <returns>使缠绕方向朝外，且方便 AddTriangle/AddQuad 入参的顶点数组索引排序</returns>
-    public static int[] SortVertices(Vector3 origin, Vector3[] vs)
+    // 判断是否 v0, v1, v2 的顺序是合适的缠绕方向
+    public static bool IsRightVSeq(Vector3 origin, Vector3 v0, Vector3 v1, Vector3 v2)
     {
-        var center = (vs[0] + vs[1] + vs[2]) / 3f;
+        var center = (v0 + v1 + v2) / 3f;
         // 决定缠绕顺序
-        var normal = GetNormal(vs[0], vs[1], vs[2]);
-        var isRightSeq = IsNormalAwayFromOrigin(center, normal, origin);
-        if (vs.Length == 3)
-            return isRightSeq
-                ? [0, 2, 1]
-                : [0, 1, 2];
-        // 目前只有 3 个顶点和 4 个顶点两种入参
-        // 四个顶点的入参时，请按照类似 [左下, 右下, 左上, 右上] 的顶点顺序传入
-        return isRightSeq
-                ? [0, 1, 2, 3]
-                : [1, 0, 3, 2];
-            
+        var normal = GetNormal(v0, v1, v2);
+        return IsNormalAwayFromOrigin(center, normal, origin);
     }
 }
