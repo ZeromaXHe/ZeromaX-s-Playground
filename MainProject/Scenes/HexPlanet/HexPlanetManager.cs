e474e6648ba43d30f31e9a39635c3e90d1349620
Zhu XH <zhuxiaohe95@163.com>
2025-03-22 14:43:43 +0800

    feature(MainProject): 天体运行 - 行星和卫星的公转自转 （行星的公转通过旋转恒星实现，自转目前暂时和分块动态加载功能冲突；新加入月球卫星）

--- MainProject/Scenes/HexPlanet/HexPlanetManager.cs
+++ MainProject/Scenes/HexPlanet/HexPlanetManager.cs
@@ -55,6 +55,8 @@ public partial class HexPlanetManager : Node3D
                 _planetAtmosphere.Set("planet_radius", _radius);
                 _planetAtmosphere.Set("atmosphere_height", _radius * 0.25f);
                 _longitudeLatitude.Draw(_radius + _planetSettingService.MaxHeight * 1.25f);
+                UpdateMoonMeshRadius(); // 卫星半径
+                UpdateLunarDist(); // 卫星轨道半径
             }
         }
     }
@@ -96,10 +98,184 @@ public partial class HexPlanetManager : Node3D
             }
         }
     }
+
     // 其实这里可以直接导入 Image, 在导入界面选择导入类型。但是导入 Image 的场景 tscn 文件会大得吓人……（等于直接按像素写一遍）
     [Export] private Texture2D _noiseSource;
     [Export] private PackedScene _unitScene;
     [Export] public ulong Seed { get; set; } = 1234;
+
+    [Export(PropertyHint.Range, "-100.0, 100.0")]
+    public float RotationTimeFactor = 1f;
+
+    private float _eclipticInclinationToGalactic = 60f;
+
+    [ExportSubgroup("行星恒星设置")]
+    [ExportToolButton("切换恒星运行状态", Icon = "DirectionalLight3D")]
+    public Callable StarMoveStatus => Callable.From(ToggleStarMoveStatus);
+
+    private void ToggleStarMoveStatus()
+    {
+        if (PlanetRevolution)
+        {
+            PlanetRevolution = false;
+            _sunRevolution.RotationDegrees = Vector3.Up * 180f;
+        }
+        else
+            PlanetRevolution = true;
+    }
+
+    [ExportToolButton("切换行星运行状态", Icon = "WorldEnvironment")]
+    public Callable PlanetMoveStatus => Callable.From(TogglePlanetMoveStatus);
+
+    private void TogglePlanetMoveStatus()
+    {
+        if (PlanetRotation)
+        {
+            PlanetRotation = false;
+            _planetAxis.Rotation = Vector3.Zero;
+        }
+        else
+            PlanetRotation = true;
+    }
+
+    // 黄道面相对银河系的银道面倾角
+    // 相关术语：银道面 Galactic Plane
+    [Export(PropertyHint.Range, "0, 180, degrees")]
+    public float EclipticInclinationToGalactic
+    {
+        get => _eclipticInclinationToGalactic;
+        set
+        {
+            _eclipticInclinationToGalactic = value;
+            if (_ready)
+                UpdateGalaxySkyRotation();
+        }
+    }
+
+    private float _planetObliquity = 23.44f;
+
+    // 行星倾角（= 黄赤交角 obliquity of the ecliptic = 23.44°）
+    // 相关术语：黄道面 Ecliptic Plane，赤道面 Earth Equatorial Plane
+    [Export(PropertyHint.Range, "0, 180, degrees")]
+    public float PlanetObliquity
+    {
+        get => _planetObliquity;
+        set
+        {
+            _planetObliquity = value;
+            if (_ready)
+            {
+                UpdateGalaxySkyRotation();
+                UpdateEclipticPlaneRotation();
+            }
+        }
+    }
+
+    [Export] public bool PlanetRevolution { get; set; } = true; // 行星公转
+
+    [Export(PropertyHint.Range, "-360, 360, degrees")]
+    public float PlanetRevolutionSpeed { get; set; } = 1f; // 行星公转速度（每秒转的度数）
+
+    [Export] public bool PlanetRotation { get; set; } = true; // 行星自转
+
+    [Export(PropertyHint.Range, "-360, 360, degrees")]
+    public float PlanetRotationSpeed { get; set; } = 10f; // 行星自转速度（每秒转的度数）
+
+    [ExportSubgroup("卫星设置")]
+    [ExportToolButton("切换卫星运行状态", Icon = "CSGSphere3D")]
+    public Callable SatelliteMoveStatus => Callable.From(ToggleSatelliteMoveStatus);
+
+    private void ToggleSatelliteMoveStatus()
+    {
+        if (SatelliteRevolution || SatelliteRotation)
+        {
+            SatelliteRevolution = false;
+            SatelliteRotation = false;
+            _lunarRevolution.RotationDegrees = Vector3.Up * 180f;
+            _moonAxis.Rotation = Vector3.Zero;
+        }
+        else
+        {
+            SatelliteRevolution = true;
+            SatelliteRotation = true;
+        }
+    }
+
+    private float _satelliteRadiusRatio = 0.273f;
+
+    // 卫星和行星的半径比
+    [Export(PropertyHint.Range, "0, 1")]
+    public float SatelliteRadiusRatio
+    {
+        get => _satelliteRadiusRatio;
+        set
+        {
+            _satelliteRadiusRatio = value;
+            if (_ready)
+                UpdateMoonMeshRadius();
+        }
+    }
+
+    private float _satelliteDistRatio = 7.5f;
+
+    // 卫星距离（轨道半径）和行星的半径比
+    // 如果大于了地日距离（行星轨道半径）的话，会被截断到小于地日距离的轨道
+    // 同样也会控制大于地球半径加卫星半径
+    [Export(PropertyHint.Range, "0, 100.0")]
+    public float SatelliteDistRatio
+    {
+        get => _satelliteDistRatio;
+        set
+        {
+            _satelliteDistRatio = value;
+            if (_ready)
+                UpdateLunarDist();
+        }
+    }
+
+    private float _satelliteObliquity = 6.68f;
+
+    // 卫星倾角
+    // 相关术语：月球轨道面（白道面）Lunar Orbit Plane，月球赤道面 Lunar Equatorial Plane，
+    // 黄白交角 obliquity of the moon path = 月球轨道倾角 Lunar Orbital Inclination = 5.14°
+    // 相对黄道的月球倾角 Lunar Obliquity to Ecliptic = 1.54°
+    [Export(PropertyHint.Range, "0, 180, degrees")]
+    public float SatelliteObliquity
+    {
+        get => _satelliteObliquity;
+        set
+        {
+            _satelliteObliquity = value;
+            if (_ready)
+                UpdateLunarObliquityRotation();
+        }
+    }
+
+    private float _satelliteOrbitInclination = 5.14f;
+
+    // 卫星轨道倾角
+    // 相关术语：黄白交角 obliquity of the moon path = 月球轨道倾角 Lunar Orbital Inclination = 5.14°
+    [Export(PropertyHint.Range, "0, 180, degrees")]
+    public float SatelliteOrbitInclination
+    {
+        get => _satelliteOrbitInclination;
+        set
+        {
+            _satelliteOrbitInclination = value;
+            if (_ready)
+                UpdateLunarOrbitPlaneRotation();
+        }
+    }
+
+    [Export] public bool SatelliteRevolution { get; set; } = true; // 卫星公转
+
+    [Export(PropertyHint.Range, "-360, 360, degrees")]
+    public float SatelliteRevolutionSpeed { get; set; } = 30f; // 卫星公转速度（每秒转的度数）
+
+    [Export] public bool SatelliteRotation { get; set; } = true; // 卫星自转
+
+    [Export(PropertyHint.Range, "-360, 360, degrees")]
+    public float SatelliteRotationSpeed { get; set; } // 卫星自转速度（每秒转的度数）
 
     private bool _ready;
 
@@ -188,6 +364,19 @@ public partial class HexPlanetManager : Node3D
     #region on-ready nodes
 
     private WorldEnvironment _worldEnvironment;
+
+    // 天体公转自转
+    private Node3D _eclipticPlane;
+    private Node3D _sunRevolution;
+    private Node3D _planetAxis;
+    private Node3D _lunarOrbitPlane;
+    private Node3D _lunarRevolution;
+    private Node3D _lunarDist;
+    private Node3D _lunarObliquity;
+    private Node3D _moonAxis;
+    private MeshInstance3D _moonMesh;
+
+    // 行星节点
     private Node3D _planetAtmosphere; // 大气层插件的 GDScript 节点
     private MeshInstance3D _groundPlaceHolder; // 球面占位网格
     private ChunkManager _chunkManager;
@@ -201,6 +390,24 @@ public partial class HexPlanetManager : Node3D
     private void InitOnReadyNodes()
     {
         _worldEnvironment = GetNode<WorldEnvironment>("%WorldEnvironment");
+        UpdateGalaxySkyRotation();
+        // 天体公转自转
+        _eclipticPlane = GetNode<Node3D>("%EclipticPlane");
+        UpdateEclipticPlaneRotation();
+        _sunRevolution = GetNode<Node3D>("%SunRevolution");
+        _planetAxis = GetNode<Node3D>("%PlanetAxis");
+        _lunarOrbitPlane = GetNode<Node3D>("%LunarOrbitPlane");
+        UpdateLunarOrbitPlaneRotation();
+        _lunarRevolution = GetNode<Node3D>("%LunarRevolution");
+        _lunarDist = GetNode<Node3D>("%LunarDist");
+        UpdateLunarDist();
+        _lunarObliquity = GetNode<Node3D>("%LunarObliquity");
+        UpdateLunarObliquityRotation();
+        _moonAxis = GetNode<Node3D>("%MoonAxis");
+        _moonMesh = GetNode<MeshInstance3D>("%MoonMesh");
+        UpdateMoonMeshRadius();
+
+        // 行星节点
         _planetAtmosphere = GetNode<Node3D>("%PlanetAtmosphere");
         _groundPlaceHolder = GetNode<MeshInstance3D>("%GroundPlaceHolder");
         _chunkManager = GetNode<ChunkManager>("%ChunkManager");
@@ -217,6 +424,26 @@ public partial class HexPlanetManager : Node3D
         }
 
         _ready = true;
+    }
+
+    private void UpdateLunarOrbitPlaneRotation() =>
+        _lunarOrbitPlane.RotationDegrees = Vector3.Right * SatelliteOrbitInclination;
+
+    private void UpdateEclipticPlaneRotation() => _eclipticPlane.RotationDegrees = Vector3.Right * PlanetObliquity;
+    private void UpdateLunarObliquityRotation() => _lunarObliquity.RotationDegrees = Vector3.Right * SatelliteObliquity;
+
+    private void UpdateLunarDist() => _lunarDist.Position =
+        Vector3.Back * Mathf.Clamp(Radius * SatelliteDistRatio, Radius * (1 + _satelliteRadiusRatio), 800f);
+
+    private void UpdateGalaxySkyRotation() =>
+        _worldEnvironment.Environment.SkyRotation =
+            Vector3.Right * Mathf.DegToRad(PlanetObliquity - EclipticInclinationToGalactic);
+
+    private void UpdateMoonMeshRadius()
+    {
+        var moonMesh = _moonMesh.Mesh as SphereMesh;
+        moonMesh?.SetRadius(Radius * SatelliteRadiusRatio);
+        moonMesh?.SetHeight(Radius * SatelliteRadiusRatio * 2);
     }
 
     #endregion
@@ -240,9 +467,11 @@ public partial class HexPlanetManager : Node3D
     public override void _Process(double delta)
     {
         if (!_ready) return;
-        _tileShaderService.UpdateData((float)delta);
+        var floatDelta = (float)delta;
+        _tileShaderService.UpdateData(floatDelta);
+        UpdateStellarRotation(floatDelta);
 
-        _lastUpdated += (float)delta;
+        _lastUpdated += floatDelta;
         if (_lastUpdated < 0.1f) return; // 每 0.1s 更新一次
         if (Mathf.Abs(_oldRadius - Radius) > 0.001f
             || _oldDivisions != Divisions
@@ -251,6 +480,27 @@ public partial class HexPlanetManager : Node3D
         if (!Engine.IsEditorHint())
             UpdateSelectTileViewer();
         _lastUpdated = 0f; // 每一秒检查一次
+    }
+
+    // 更新天体旋转
+    private void UpdateStellarRotation(float delta)
+    {
+        // 行星公转
+        if (PlanetRevolution)
+            _sunRevolution.RotationDegrees = RotationTimeFactor * Vector3.Up * Mathf.Wrap(
+                _sunRevolution.RotationDegrees.Y + PlanetRevolutionSpeed * delta, 0f, 360f);
+        // 行星自转
+        if (PlanetRotation)
+            _planetAxis.RotationDegrees = RotationTimeFactor * Vector3.Up * Mathf.Wrap(
+                _planetAxis.RotationDegrees.Y + PlanetRotationSpeed * delta, 0f, 360f);
+        // 卫星公转
+        if (SatelliteRevolution)
+            _lunarRevolution.RotationDegrees = RotationTimeFactor * Vector3.Up * Mathf.Wrap(
+                _lunarRevolution.RotationDegrees.Y + SatelliteRevolutionSpeed * delta, 0f, 360f);
+        // 卫星自转
+        if (SatelliteRotation)
+            _moonAxis.RotationDegrees = RotationTimeFactor * Vector3.Up * Mathf.Wrap(
+                _moonAxis.RotationDegrees.Y + SatelliteRotationSpeed * delta, 0f, 360f);
     }
 
     private void UpdateSelectTileViewer()
