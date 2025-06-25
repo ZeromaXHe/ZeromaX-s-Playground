bd83ce2a1459dbf65c5e33de8add166b9ce01c78
Zhu XH <zhuxiaohe95@163.com>
2025-06-25 09:09:54 +0800

    refactor(TerraObserver): 将 HexMeshFS、HexGridChunkFS 多例节点还原为 C# 节点代码

--- TerraObserver/Scenes/Chunks/Views/HexGridChunk.cs
+++ TerraObserver/Scenes/Chunks/Views/HexGridChunk.cs
@@ -1,7 +1,8 @@
 using System;
 using Godot;
 using TO.Abstractions.Views.Chunks;
-using TO.Presenters.Views.Chunks;
+using TO.Domains.Enums.HexSpheres.Chunks;
+using TO.Domains.Enums.Meshes;
 
 namespace TerraObserver.Scenes.Chunks.Views;
 
@@ -9,14 +10,8 @@ namespace TerraObserver.Scenes.Chunks.Views;
 /// Author: Zhu XH (ZeromaXHe)
 /// Date: 2025-06-06 16:31:36
 [Tool]
-public partial class HexGridChunk : HexGridChunkFS, IHexGridChunk
+public partial class HexGridChunk : Node3D, IHexGridChunk
 {
-    #region 生命周期
-
-    public override void _Process(double delta) => Processed?.Invoke(this);
-
-    #endregion
-
     #region 事件
 
     public event Action<IHexGridChunk>? Processed;
@@ -26,17 +21,72 @@ public partial class HexGridChunk : HexGridChunkFS, IHexGridChunk
     #region Export 属性
 
     [Export] public HexMesh? Terrain { get; set; }
-    public override IHexMesh? GetTerrain() => Terrain;
+    public IHexMesh? GetTerrain() => Terrain;
     [Export] public HexMesh? Rivers { get; set; }
-    public override IHexMesh? GetRivers() => Rivers;
+    public IHexMesh? GetRivers() => Rivers;
     [Export] public HexMesh? Roads { get; set; }
-    public override IHexMesh? GetRoads() => Roads;
+    public IHexMesh? GetRoads() => Roads;
     [Export] public HexMesh? Water { get; set; }
-    public override IHexMesh? GetWater() => Water;
+    public IHexMesh? GetWater() => Water;
     [Export] public HexMesh? WaterShore { get; set; }
-    public override IHexMesh? GetWaterShore() => WaterShore;
+    public IHexMesh? GetWaterShore() => WaterShore;
     [Export] public HexMesh? Estuary { get; set; }
-    public override IHexMesh? GetEstuary() => Estuary;
+    public IHexMesh? GetEstuary() => Estuary;
 
     #endregion
+
+    #region 普通属性
+
+    public int Id { get; set; }
+    public ChunkLodEnum Lod { get; set; } = ChunkLodEnum.JustHex;
+
+    #endregion
+
+    #region 生命周期
+
+    public override void _Process(double delta) => Processed?.Invoke(this);
+
+    #endregion
+
+    public void HideOutOfSight()
+    {
+        Hide();
+        Terrain!.Clear();
+        Rivers!.Clear();
+        Roads!.Clear();
+        Water!.Clear();
+        WaterShore!.Clear();
+        Estuary!.Clear();
+        Id = 0; // 重置 id，归还给池子
+    }
+
+    public void ShowMesh(Mesh[] meshes)
+    {
+        Terrain!.ShowMesh(meshes[(int)MeshType.Terrain]);
+        Water!.ShowMesh(meshes[(int)MeshType.Water]);
+        WaterShore!.ShowMesh(meshes[(int)MeshType.WaterShore]);
+        Estuary!.ShowMesh(meshes[(int)MeshType.Estuary]);
+    }
+
+    public Mesh[] GetMeshes() => [Terrain!.Mesh, Water!.Mesh, WaterShore!.Mesh, Estuary!.Mesh];
+
+    public void ApplyNewData()
+    {
+        Terrain!.Apply();
+        Rivers!.Apply(); // 河流暂时不支持 Lod
+        Roads!.Apply(); // 道路暂时不支持 Lod
+        Water!.Apply();
+        WaterShore!.Apply();
+        Estuary!.Apply();
+    }
+
+    public void ClearOldData()
+    {
+        Terrain!.Clear();
+        Rivers!.Clear();
+        Roads!.Clear();
+        Water!.Clear();
+        WaterShore!.Clear();
+        Estuary!.Clear();
+    }
 }
